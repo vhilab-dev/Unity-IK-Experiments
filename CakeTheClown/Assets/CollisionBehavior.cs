f70f5ae89515b346ad826e9f15e1f3c2bc508fb4
VHIL <vhilab@gmail.com>
2017-08-14 13:39:54 -0700

    Added Jerry's animation example

---
+++ CakeTheClown/Assets/CollisionBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionBehavior : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision){
+		GameObject clown = transform.parent.gameObject;
+		Animator clownAnimator = clown.GetComponent<Animator> ();
+		clownAnimator.SetTrigger ("hit");
+
+	}
+}
